c76e5d9b63812335cc22248d9afd375336491998
abhineet123 <asingh1@ualberta.ca>
2022-11-11 21:26:21 +0000

    minor fix

--- projects/SeqFormer/seqformer/data/datasets/builtin.py
+++ projects/SeqFormer/seqformer/data/datasets/builtin.py
@@ -59,11 +59,11 @@ _PREDEFINED_SPLITS_YTVIS_2019 = {
     ),
     "ytvis-ipsc-all_frames_roi_g2_39_53": (
         "ipsc/well3/all_frames_roi",
-        "ipsc/well3/all_frames_roi/ytvis19/ytvis-ipsc-all_frames_roi_g2_39_53-test.json"
+        "ipsc/well3/all_frames_roi/ytvis19/ytvis-ipsc-all_frames_roi_g2_39_53.json"
     ),
     "ytvis-ipsc-all_frames_roi_g2_seq_1_39_53": (
         "ipsc/well3/all_frames_roi",
-        "ipsc/well3/all_frames_roi/ytvis19/ytvis-ipsc-all_frames_roi_g2_seq_1_39_53-train.json"
+        "ipsc/well3/all_frames_roi/ytvis19/ytvis-ipsc-all_frames_roi_g2_seq_1_39_53.json"
     ),
 }
 
